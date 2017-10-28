a6f43f5057004c1c892576a55cac0090bd10759f
Josh NG <joshng@hotmail.co.uk>
2017-10-28 18:40:22 +0100

    Connect.php
    Added Connect.php for when we login and register. Might need to change this as Register will create connection, then it will have to disconnect after adding user and make them sign in.

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php	
+
+$host = "silva.computing.dundee.ac.uk";
+$username = "17ac3u07";
+$password = "cba123";
+$database = "17ac3d07";
+//will change to getting the user name and password 
+
+
+try{
+
+	$mysql = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
+	
+} catch (PDOException $e){
+	// Could not connect
+	echo "Database Connection Error!";
+	var_dump($e);
+	die();
+}
+
+
+$mysql = new PDO("mysql:host=".$host, $username, $password);
+$mysql->exec("USE databaseName");
+
+?>
