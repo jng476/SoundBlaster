b53ec262beeddd109fbb5faa227d0df5a93a6af7
Martin <martinlearmont@hotmail.com>
2017-11-07 11:09:34 +0000

    Changed login.php to include navbar and script

--- login.php
+++ login.php
@@ -1,41 +1,37 @@
-<?php 
+<?php
 session_start();
 if(isset($_SESSION['login'])){
-	if($_SESSION['login'] == "Logged in"){
-			
-		header("Location: searchTable.php");
-		exit();
-	}
+    if($_SESSION['login'] == "Logged in"){
+        header("Location: searchTable.php");
+        exit();
+    }
 }
-
-?> 
-
+?>
 
 <html>
-	<head>
-  <title>SoundBlaster Login</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-		<div class="container">
-			<h2>Login</h2>
-			<form method="post" action="login2.0.php">
-				<div class="form-group">
-				<label for "username">Username</label>
-				<input type="text" calss="form-control" size="35" name="username"><br/>
-			</div>
-			<div class="form-group">
-				<label for "password">Password</label>
-				<input type="password" class="form-control" size="35" name="password"><br/>
-			</div>
-				<input type="submit" value="Login">
-				<?php if(isset($_SESSION['login'])){ ?>
-				<td style='colour:red;'> <?php echo  $_SESSION['login']; session_destroy();} ?> </td>
-			</form>
-		</div>
-	</body>
+    <head>
+        <title>SoundBlaster Login</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php include 'scripts.html'?>
+    </head>
+    <body>
+        <?php include 'navigation.php' ?>
+        <div class="container">
+            <h2>Login</h2>
+            <form method="post" action="login2.0.php">
+                <div class="form-group">
+                    <label for "username">Username</label>
+                    <input type="text" calss="form-control" size="35" name="username"><br/>
+                </div>
+                <div class="form-group">
+                    <label for "password">Password</label>
+                    <input type="password" class="form-control" size="35" name="password"><br/>
+                </div>
+                <input type="submit" value="Login">
+                <?php if(isset($_SESSION['login'])){ ?>
+                    <td style='colour:red;'> <?php echo  $_SESSION['login']; session_destroy();} ?> </td>
+            </form>
+        </div>
+    </body>
 </html>
