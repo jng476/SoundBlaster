267c0f4e3748b707aec7d276f0e3c94e4a73a817
Josh NG <joshng@hotmail.co.uk>
2017-11-08 22:22:19 +0000

    Suppliers
    Lists the suppliers and in the list has a little click url that will send you to another page to see what products the suppliers have.

---
+++ viewSupplier.php
@@ -0,0 +1,73 @@
+<?php 
+// https://zeno.computing.dundee.ac.uk/2017-ac32006/joshng/viewSupplier.php?supplierID=1 
+// link above for the get. 
+include 'connect.php';
+if($_SESSION['login']!="Logged in"){
+	header("Location: login.php");
+	die();
+}
+$query = "SELECT Name, Description, SupplierCost FROM Product WHERE SupplierID = ".$_GET['supplierID'];
+$stmt = $mysql->prepare($query);
+$stmt->execute();
+
+?> 
+
+<html>
+	<head>
+		 <title>Products</title>		
+		<?php include 'scripts.html'?>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		
+	</head>
+	<body>
+		
+				
+				
+		<div id="container">
+						
+			<div id="nav">
+ 				<?php include 'navigation.php'; ?>
+			</div>
+			
+			
+			<div id="content">
+				
+				
+				<div id="main">
+					</br>
+					</br>
+					
+					<div id="bubbleText">
+					<table>
+					<thead>
+						<h2><u> Products </u></h2>
+						</br>
+						<table id="basketTable">
+						<thead>
+						<td>Name</td>
+						<td>Description</td>
+						<td>Supplier Price</td>
+						</thead>
+		
+						<tbody?>
+						<?php foreach($stmt->fetchAll() as $result): ?>
+							<tr>
+							<td><?php echo $result['Name']; ?></td>
+							<td><?php echo $result['Description']; ?></td>
+							<td><?php echo $result['SupplierCost']; ?></td>
+							</tr>
+						<?php endforeach; ?>
+						</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+		</br>
+		</br>
+		</br>
+		<div id="footer">
+				Created in 2017 by Team 7
+		</div>
+	</body>
+	</html>
