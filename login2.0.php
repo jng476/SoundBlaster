b0cc628be688270414db2ce93c0d148d3d7a32d2
Martin <martinlearmont@hotmail.com>
2017-11-13 11:25:17 +0000

    fixed login2.0 changes

--- login2.0.php
+++ login2.0.php
@@ -1,38 +1,41 @@
-<?php	
+<?php
 
-$host = "silva.computing.dundee.ac.uk";
-$username = "17ac3u07";
-$password = "cba123";
-$database = "17ac3d07";
-//will change to getting the user name and password 
-session_start();
+include 'connect.php';
 
-try{
-    global $mysql;
-	$mysql = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
-    #$mysql->exec('USE 17ac3d07');
-    $prepared = $mysql->prepare('SELECT * FROM useraccount where username= :username AND password = :password');
-    $prepared->bindParam(':username', $_POST['username']);
-    $prepared->bindParam(':password', $_POST['password']);
-    $prepared->execute();
-    
-    if ($prepared->rowCount() != 0) {
+$prepared = $mysql->prepare('SELECT * FROM useraccount where username= :username AND password = :password');
+$prepared->bindParam(':username', $_POST['username']);
+$prepared->bindParam(':password', $_POST['password']);
+$prepared->execute();
+
+if ($prepared->rowCount() != 0) {
     $_SESSION['login'] = "Logged in";
     $_SESSION['username']=$_POST['username'];
-    header("Location: index.php");
+    $_SESSION['permissions'] = array();
+    $prepPermissions = $mysql->prepare('select p.`Name`, uap.`AccessLevel` from useraccountpermission as uap join permission as p on uap.PermissionID = p.ID join useraccount as ua on uap.UserAccountID = ua.ID where ua.username=:username;');
+    $prepPermissions->bindParam(':username', $_POST['username']);
+    $prepPermissions->execute();
+    $permissionResult = $prepPermissions ->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($permissionResult as $row) {
+        $_SESSION['permissions'][$row['Name']] = $row['AccessLevel'];
     }
-    else {
-        echo "Incorrect username or password";
-        $_SESSION['login'] = "incorrect username or password";
-        header("Location: login.php");
+
+    $prepGroupPermissions=$mysql->prepare('select p.`Name`, ugp.`AccessLevel` from permission as p join usergrouppermission as ugp on p.ID = ugp.PermissionID join usergroupaccount as uga on uga.UserGroupID=ugp.UserGroupID join useraccount as ua on uga.UserAccountID = ua.ID where ua.username=:username;');
+    $prepGroupPermissions->bindParam(':username', $_POST['username']);
+    $prepGroupPermissions->execute();
+    $prepGroupResult = $prepGroupPermissions -> fetchAll(PDO::FETCH_ASSOC);
+
+
+    foreach($prepGroupResult as $row)  {
+        if(!key_exists($_SESSION, $row['Name']) || $row['AccessLevel'] > $_SESSION['permissions']['Name'])
+            $_SESSION['permissions'][$row['Name']] = $row['AccessLevel'];
     }
-} catch (PDOException $e){
-	// Could not connect
-	
-  	$_SESSION['login'] = "incorrect username or password";
-	echo "Database Connection Error!";
-	var_dump($e);
-	header("Location: login.php");
-	die();
+    header("Location: searchTable.php");
 }
+else {
+    echo "Incorrect username or password";
+    $_SESSION['login'] = "incorrect username or password";
+    #header("Location: login.php");
+}
+
 ?>
