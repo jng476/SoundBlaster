f77f31747dd849076517e3be0b77a2ce4fb52594
suchsen <33463637+suchsen@users.noreply.github.com>
2017-11-17 03:17:04 +0000

    permission to permissions

--- check-authorisation.php
+++ check-authorisation.php
@@ -6,7 +6,7 @@ if($_SESSION['login']!="Logged in"){
     header("Location: login.php");
 }
 #Checks the permissions array of the parent php file and makes sure the session includes them
-if(isset($permission)){
+if(isset($permissions)){
 	foreach ($permissions as $permission => $level) {
 		if (!key_exists($permission, $_SESSION['permissions']) || $_SESSION['permissions'][$permission] < $level){
 			header("Location: access-denied.php");
